79b1b36ec6a61e2b36800f1d6921845344943b25
KumarS14 <sonjaykumar1404@gmail.com>
2023-05-30 16:51:37 +0100

    created a test menu and test that checks menu

--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -1,6 +1,19 @@
 require 'menu.rb'
 RSpec.describe Menu do
+    test_menu = {
+        chips: 2.99 ,
+        pizza: 3.99 ,
+        burger: 5.99 ,
+        salmon: 6 ,
+        praws: 2.47 ,
+        steak: 11.99 ,
+        lamb_chops: 8.99
+       }
     it 'Should create a instance of menu' do
        expect(menu = Menu.new).to eq(menu)
     end
+    it 'Should return the menu' do
+        the_menu = Menu.new
+        expect(the_menu.menu).to eq(test_menu)
+     end
 end
