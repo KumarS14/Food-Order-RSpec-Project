5bf546abf2723b21a578c3b0d4a671c652559bb9
KumarS14 <sonjaykumar1404@gmail.com>
2023-05-30 16:51:11 +0100

    created menu method that returns the menu

--- lib/menu.rb
+++ lib/menu.rb
@@ -4,8 +4,15 @@ class Menu
          chips: 2.99 ,
          pizza: 3.99 ,
          burger: 5.99 ,
-         salmon: 6 
+         salmon: 6 ,
+         praws: 2.47 ,
+         steak: 11.99 ,
+         lamb_chops: 8.99
         }
+    end
+    def menu
+        return @the_menu
+
     end
 
 
