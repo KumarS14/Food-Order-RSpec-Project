7c69139501681b536974f91ac8f06a6c7b1d6a8f
KumarS14 <sonjaykumar1404@gmail.com>
2023-05-31 12:30:13 +0100

    method that returns bill with total at the end

--- lib/receipt.rb
+++ lib/receipt.rb
@@ -2,6 +2,12 @@ class Receipt
    def initialize(order)
         @order = order.current_order
     end
-   
+   def total
+    the_total = []
+    @order.each{|k,v| the_total << "You have orderd #{k} for $#{v.to_f}" }
+    the_total <<  "The total comes down to: $#{@order.values.sum}"
+    the_total << "Thank you for your time please come back again soon."
+    return the_total.join("\n")
+   end
 
 end
