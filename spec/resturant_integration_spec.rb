c285eec4e9d4e106c96f7a8f07f2090b0ed98ba4
KumarS14 <sonjaykumar1404@gmail.com>
2023-05-31 11:05:24 +0100

    testto see if user item can be matched and stored

--- spec/resturant_integration_spec.rb
+++ spec/resturant_integration_spec.rb
@@ -10,7 +10,13 @@ RSpec.describe 'Integration' do
         the_menu = Menu.new
         item_order = Order.new(the_menu)
         #item_order.order("biscuit")
-        expect { item_order.order("biscuit") }.to raise_error('This item does not exist')
+        expect { item_order.place_order("biscuit") }.to raise_error('This item does not exist')
+    end
+      it 'should allow the user to order an item and fail as the item does not exist' do
+        the_menu = Menu.new
+        item_order = Order.new(the_menu)
+         item_order.place_order("chips")
+         expect(item_order.current_order).to eq("chips for: 2.99")
       end
     
  
