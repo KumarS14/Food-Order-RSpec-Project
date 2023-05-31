66f1ed85fd5c3dfccd017dae9ab2418a912b2a9e
KumarS14 <sonjaykumar1404@gmail.com>
2023-05-31 10:49:17 +0100

    test error is raised if user input not on menu

--- spec/resturant_integration_spec.rb
+++ spec/resturant_integration_spec.rb
@@ -6,6 +6,12 @@ RSpec.describe 'Integration' do
         the_menu = Menu.new
         expect(item_order = Order.new(the_menu)).to be_instance_of(Order)
     end
+    it 'should allow the user to order an item and fail as the item does not exist' do
+        the_menu = Menu.new
+        item_order = Order.new(the_menu)
+        #item_order.order("biscuit")
+        expect { item_order.order("biscuit") }.to raise_error('This item does not exist')
+      end
     
  
 end
