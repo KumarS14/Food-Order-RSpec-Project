16ccf1d5cb6856cd8b4ca6f12606061113ee5158
KumarS14 <sonjaykumar1404@gmail.com>
2023-05-31 15:55:29 +0100

    mock test that returns the reciept

--- spec/receipt_spec.rb
+++ spec/receipt_spec.rb
@@ -6,5 +6,12 @@ RSpec.describe Receipt do
         expect(receipt = Receipt.new(mock_order_class)).to be_instance_of(Receipt)
 
     end
+    it "should be able to mock initalize the order class and return a reciept" do
+        mock_order_class = double("order_class")
+        allow(mock_order_class).to receive(:current_order).and_return("burger" => 5.99,"chips" => 2.99,"pizza" => 3.99,)
+        receipt = Receipt.new(mock_order_class)
+        expect(receipt.total).to eq("You have orderd burger for $5.99\nYou have orderd chips for $2.99\nYou have orderd pizza for $3.99\nThe total comes down to: $12.97\nThank you for your time please come back again soon." )
+
+    end
 
 end
