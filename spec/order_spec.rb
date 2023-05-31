9163c7786596df96dced8fa395002050325a237b
KumarS14 <sonjaykumar1404@gmail.com>
2023-05-31 14:01:39 +0100

    test to mock menu and return it in order

--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -1,6 +1,6 @@
 require 'order.rb'
 RSpec.describe Order do
-    test_menu = {
+    mock_menu = {
         chips: 2.99 ,
         pizza: 3.99 ,
         burger: 5.99 ,
@@ -11,13 +11,20 @@ RSpec.describe Order do
        }
     it 'Should mock menu going into order' do
         mock_order = double(:the_menu)
-        allow(mock_order).to receive(:menu).and_return(test_menu)
+        allow(mock_order).to receive(:menu).and_return(mock_menu)
         expect(order = Order.new(mock_order)).to be_instance_of(Order)
     end
     it 'Should mock menu going into order' do
         mock_order = double(:the_menu)
-        allow(mock_order).to receive(:menu).and_return(test_menu)
+        allow(mock_order).to receive(:menu).and_return(mock_menu)
         order = Order.new(mock_order)
         expect{order.place_order("fish")}.to raise_error("This item does not exist")
+    end
+    it 'Should mock menu going into order' do
+        mock_order = double(:the_menu)
+        allow(mock_order).to receive(:menu).and_return(mock_menu)
+        order = Order.new(mock_order)
+        order.place_order("chips")
+       expect(order.current_order).to eq("chips" => 2.99,)
     end
 end
