63941b90da4e1688189edec7a2cdb25d347dd312
KumarS14 <sonjaykumar1404@gmail.com>
2023-05-31 11:05:33 +0100

    method that stores user input

--- lib/order.rb
+++ lib/order.rb
@@ -1,10 +1,14 @@
 class Order
     def initialize(menu)
         @menu = menu.menu
+        @order = []
     end
-    def order(item)
+    def place_order(item)
         raise 'This item does not exist' unless @menu.has_key?(item.to_sym)
-
+        @order << "#{item} for: #{@menu.fetch(item.to_sym)}"
+    end
+    def current_order
+        return @order.join
     end
 
 
