e90b613bd28891ea13f7d6824aca1c899776ed06
KumarS14 <sonjaykumar1404@gmail.com>
2023-05-31 11:45:11 +0100

    test if user is able to remove item from order

--- spec/resturant_integration_spec.rb
+++ spec/resturant_integration_spec.rb
@@ -28,7 +28,7 @@ RSpec.describe 'Integration' do
          expect(item_order.current_order).to include("chips")
          expect(item_order.current_order).to include("pizza")
       end
-      it 'should allow the user to order two valid items but not the third as that is not on the menu' do
+    it 'should allow the user to order two valid items but not the third as that is not on the menu' do
         the_menu = Menu.new
         item_order = Order.new(the_menu)
          item_order.place_order("chips")
@@ -36,7 +36,20 @@ RSpec.describe 'Integration' do
          expect { item_order.place_order("biscuit") }.to raise_error('This item does not exist')
          expect(item_order.current_order).to include("chips")
          expect(item_order.current_order).to include("burger")
-      end
+    end
+    it 'should allow the user to order three valid items and let them remove one of the items' do
+        the_menu = Menu.new
+        item_order = Order.new(the_menu)
+        item_order.place_order("chips")
+        item_order.place_order("burger")
+        item_order.place_order("pizza")
+        item_order.place_order("steak")
+        item_order.delete_item("pizza")
+        expect(item_order.current_order).to include("burger")
+        expect(item_order.current_order).to include("chips")
+        expect(item_order.current_order).to include("steak")
+        
+    end
      
      
     
